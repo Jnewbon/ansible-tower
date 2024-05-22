01fa3ab461bdc4e37f0ea649e949f5c0574cb79d
Jnewbon <48688400+Jnewbon@users.noreply.github.com>
2024-05-22 23:32:59 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -36,26 +36,3 @@ EOL
 
 chmod 700 /opt/ansible-tower-password
 chmod 700 /opt/ansible-tower/inventory
-
-# Locales for postgres failed execution in tower install
-cat >/etc/default/locale <<EOL
-LANG=en_US.UTF-8
-LANGUAGE=
-LC_CTYPE="en_US.UTF-8"
-LC_NUMERIC="en_US.UTF-8"
-LC_TIME="en_US.UTF-8"
-LC_COLLATE="en_US.UTF-8"
-LC_MONETARY="en_US.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-LC_PAPER="en_US.UTF-8"
-LC_NAME="en_US.UTF-8"
-LC_ADDRESS="en_US.UTF-8"
-LC_TELEPHONE="en_US.UTF-8"
-LC_MEASUREMENT="en_US.UTF-8"
-LC_IDENTIFICATION="en_US.UTF-8"
-LC_ALL=en_US.UTF-8
-EOL
-
-locale-gen en_US.UTF-8
-dpkg-reconfigure locales
-update-locale
